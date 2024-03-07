b7c542d5647a28b85f5f16de489958c80348df5e
nyangbin <nyangbin@gmail.com>
2024-03-07 19:48:49 +0800

    feat: py script

---
+++ UiClient/UI.py
@@ -0,0 +1,37 @@
+import streamlit as st
+import numpy as np
+import time
+import pandas as pd
+
+st.write("Here's our first attempt at using data to create a table:")
+st.write(pd.DataFrame({
+    'first column': [1, 2, 3, 4],
+    'second column': [10, 20, 30, 40]
+}))
+
+name_input = st.text_input("Your name", key="name")
+
+if name_input:
+    st.write(f"You said: {name_input}")
+
+
+# progress_bar = st.sidebar.progress(0)
+# status_text = st.sidebar.empty()
+# last_rows = np.random.randn(1, 1)
+# chart = st.line_chart(last_rows)
+
+# for i in range(1, 101):
+#     new_rows = last_rows[-1, :] + np.random.randn(5, 1).cumsum(axis=0)
+#     status_text.text("%i%% Complete" % i)
+#     chart.add_rows(new_rows)
+#     progress_bar.progress(i)
+#     last_rows = new_rows
+#     time.sleep(0.05)
+
+# progress_bar.empty()
+
+# # Streamlit widgets automatically run the script from top to bottom. Since
+# # this button is not connected to any other logic, it just causes a plain
+# # rerun.
+# st.button("Re-run")
+
