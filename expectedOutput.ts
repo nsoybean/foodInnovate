ce08742eb102bffaae55c9730fc287cfd2a7d80e
nyangbin <nyangbin@gmail.com>
2024-03-07 18:05:16 +0800

    feat: data model

--- expectedOutput.ts
+++ expectedOutput.ts
@@ -1,8 +1,20 @@
 const reviews = [
   {
+    id: "32424234",
+    // ML
     sentiment: "postive/negative",
     emotion: "sad/happy",
     tags: ["N1", "N2"],
-    score: 0.5, // out of google review
+    gender: "guy/girl",
+
+    // out of google review
+    name: "sad",
+    text: "",
+    score: 0.5,
+    stars: 6,
+    date: "date",
+
+    // vector
+    vectorId: "asdasdas",
   },
 ];
